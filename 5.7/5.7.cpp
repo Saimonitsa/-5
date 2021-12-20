06cece6400486c3c0e7c2cbaa2d5a22fce427447
Saimonitsa <95011357+Saimonitsa@users.noreply.github.com>
2021-12-21 00:42:28 +0500

    Update 5.7.cpp

--- 5.7/5.7.cpp
+++ 5.7/5.7.cpp
@@ -11,4 +11,21 @@ int main() {
 	p2 = move(p1);
 
 	p2.swap(p1);
+
+	unique_ptr<int> p3(new int(5));
+	int* p = p3.get();
+
+	int* p4 = new int(6);
+	unique_ptr<int> p5(p4);
+	p5.reset();
+
+	int* p6 = new int(7);
+	unique_ptr<int> p7(p6);
+	p7.release();
+
+	shared_ptr<int> sh1(new int(5));
+	shared_ptr<int> sh2(sh1);
+
+	return 0;
+
 }
