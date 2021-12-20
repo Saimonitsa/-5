06b0ef96bb56ecf7817b70da3b4114bc14bbe445
Saimonitsa <95011357+Saimonitsa@users.noreply.github.com>
2021-12-20 21:06:48 +0500

    Добавьте файлы проекта.

---
+++ лаба ооп5/лаба ооп5.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class Animal {
+public:
+	void sound() {
+		printf("Animal::sound\n");
+	}
+	void sound1() {
+		printf("!!\n");
+	}
+};
+
+class Cat : public Animal {
+public:
+	void catch_() {
+		printf("Cat::catch_\n");
+	}
+	void sound1() {
+		printf("mew-mew\n");
+	}
+};
+
+int main() {
+	Animal* a = new Cat;
+	Cat* c = new Cat;
+	a->sound();
+	c->catch_();
+	c->sound();
+
+	a->sound1();
+	c->sound1();
+}
