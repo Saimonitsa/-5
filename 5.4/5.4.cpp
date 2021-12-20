6af69c3a6d3c33a44649f5b09f4ed7fd960ab93e
Saimonitsa <95011357+Saimonitsa@users.noreply.github.com>
2021-12-20 21:24:49 +0500

    p8

--- 5.4/5.4.cpp
+++ 5.4/5.4.cpp
@@ -1,1 +1,36 @@
-﻿
+﻿#include <iostream>
+
+using namespace std;
+
+class Animal {
+public:
+	virtual string classname() {
+		return "Animal";
+	}
+	virtual bool isA(string who) {
+		return (who == "Animal");
+	}
+};
+
+class Cat : public Animal {
+public:
+	void catch_() {
+		printf("Cat::catch_\n");
+	}
+	string classname() {
+		return "Cat";
+	}
+	bool isA(string who) {
+		return (who == "Cat" || who == "Animal");
+	}
+};
+
+class Dog : public Animal {
+public:
+	string classname() {
+		return "Dog";
+	}
+	bool isA(string who) {
+		return (who == "Dog" || who == "Animal");
+	}
+};
