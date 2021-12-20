da836418534335f03f94ad9aa19b9d0540d2e199
Saimonitsa <95011357+Saimonitsa@users.noreply.github.com>
2021-12-20 21:12:51 +0500

    Update 5.2.cpp

--- 5.2/5.2.cpp
+++ 5.2/5.2.cpp
@@ -1,1 +1,23 @@
-﻿
+﻿#include <iostream>
+
+using namespace std;
+
+class Base {
+public:
+
+	void foo() {
+		printf("Base\n");
+	}
+};
+
+class Desc : public Base {
+public:
+
+	void foo() {
+		printf("Desc\n");
+	}
+};
+
+int main() {
+	Base* x = new Base();
+}
