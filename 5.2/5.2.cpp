0a8c9510975185262b0581c66d77780d833fb3af
Saimonitsa <95011357+Saimonitsa@users.noreply.github.com>
2021-12-20 21:17:05 +0500

    p3

--- 5.2/5.2.cpp
+++ 5.2/5.2.cpp
@@ -6,6 +6,7 @@ class Base {
 public:
 
 	void foo() {
+
 		printf("Base\n");
 	}
 };
@@ -14,11 +15,13 @@ class Desc : public Base {
 public:
 
 	void foo() {
+
 		printf("Desc\n");
 	}
 };
 
 int main() {
+
 	Base* x = new Base();
 	x->foo();
 
@@ -27,4 +30,6 @@ int main() {
 
 	Base* x2 = new Desc();
 	x2->foo();
+
+
 }
